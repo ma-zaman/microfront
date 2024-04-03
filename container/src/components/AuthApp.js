b59fb29aca9d0a117ca5c5d3528db2c8a43bf275
Ali Zaman <ali.zaman.ext@orange.com>
2024-04-03 14:51:23 +0200

    feat: add isSignedIn state

--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -3,7 +3,7 @@ import { mount } from 'auth/AuthApp'
 import React, { useRef, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null)
   const history = useHistory()
 
@@ -14,9 +14,7 @@ export default () => {
         const { pathname } = history.location
         if (pathname !== nextPathname) history.push(nextPathname)
       },
-      onSignIn: () => {
-        console.log('User signed in')
-      }
+      onSignIn
     })
 
     history.listen(onParentNavigate)
